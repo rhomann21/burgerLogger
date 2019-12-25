8a39fe1294f62ff9c72fceba58b8d09ac7684104
Rachael Homann <rachaelhomann@gmail.com>
2019-12-25 12:53:49 -0600

    working on stylizing issues

--- burgerLog/controllers/burger_controller.js
+++ burgerLog/controllers/burger_controller.js
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burgers.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
